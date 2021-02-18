dbfd7635d9d5fb9912015ce9a6c2f9a99f243947
Diego <A01552594@itesm.mx>
2021-02-17 18:29:51 -0600

    Laboratorio 5

---
+++ Laboratorio 5/JS/main.js
@@ -0,0 +1,21 @@
+//function myFunction() {
+//  document.getElementById("myDIV").style.position = "absolute";
+//}
+//
+//function Ocultar() {
+//  document.getElementById("myP").style.visibility = "hidden";
+//}
+
+function mostrar1() {
+    let res1 = document.getElementById("r1");
+    res1.innerHTML = "<p>Para garantizar una buena experiencia de usuario; mediante la detección de datos no válidos en el lado del cliente, el usuario puede corregirlos de inmediato</p>";
+}
+
+function mostrar2() {
+    let res2 = document.getElementById("r2");
+    res2.innerHTML = "<p>Desactivando javaScript del navegador</p>";
+}
+function mostrar3() {
+    let res3 = document.getElementById("r3");
+    res3.innerHTML = "<p>Porque es para el usuario común, ademas de que te ayuda a la reduccion de errores</p>";
+}
